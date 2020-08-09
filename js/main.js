fcbf74dcca3e8886b2e235ae62b1bb3a89b023d8
shaggy611 <sevczik11@gmail.com>
2020-08-10 02:15:55 +0300

    template

---
+++ js/main.js
@@ -0,0 +1,69 @@
+$(document).ready(function () {
+
+    (function($) {
+      var IS_IOS = /iphone|ipad/i.test(navigator.userAgent);
+      $.fn.nodoubletapzoom = function() {
+        if (IS_IOS)
+          $(this).bind('touchstart', function preventZoom(e) {
+            var t2 = e.timeStamp
+              , t1 = $(this).data('lastTouch') || t2
+              , dt = t2 - t1
+              , fingers = e.originalEvent.touches.length;
+            $(this).data('lastTouch', t2);
+            if (!dt || dt > 500 || fingers > 1) return;
+    
+            e.preventDefault();
+            $(this).trigger('click').trigger('click');
+          });
+      };
+    })(jQuery);
+
+   // SLIDER INITIALIZATION==================================================
+   $(".js-slider").slick({
+    dots: true,
+    arrows: false,
+    infinite: true,
+    speed: 300,
+    slidesToShow: 3,
+    slidesToScroll: 1,
+    autoplay: true,
+    autoplaySpeed: 3000,
+    responsive: [
+      {
+        breakpoint: 1200,
+        settings: {
+          slidesToShow: 2,
+          slidesToScroll: 1,
+          infinite: true,
+          dots: true
+        }
+      },
+      {
+        breakpoint: 1024,
+        settings: {
+          slidesToShow: 2,
+          slidesToScroll: 1,
+          infinite: true,
+          dots: true
+        }
+      },
+      {
+        breakpoint: 767,
+        settings: {
+          slidesToShow: 1,
+          slidesToScroll: 1,
+          dots: true
+        }
+      },
+      {
+        breakpoint: 480,
+        settings: {
+          slidesToShow: 1,
+          slidesToScroll: 1,
+          dots: true
+        }
+      }
+    ]
+  });
+// ===========================================================================
+});  
