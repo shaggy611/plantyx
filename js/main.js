d7b4a137a3d203900c62d97b5396433e9d056747
shaggy611 <sevczik11@gmail.com>
2020-08-18 20:10:47 +0300

    svg

--- js/main.js
+++ js/main.js
@@ -66,5 +66,13 @@ $(document).ready(function () {
     ]
   });
 // ===========================================================================
+setInterval(function rewrite() {
+  let a = document.getElementById("svg-container").innerHTML = "<svg viewBox='0 0 1280 371' fill='none' xmlns='http://www.w3.org/2000/svg' class='header__svg' preserveAspectRatio='xMinYMin meet'><path  class='line7' d='M1098 133.303L1109.26 141.351L1109.25 125.247L1098 133.303Z'/><line class='line6' x2='1109' y2='133' x1='1278' y1='133' stroke-width='2'/><line class='line5' x1='1279' y1='368' x2='1279' y2='132' stroke-width='2'/><line class='line4' x2='1280' y2='369.114' x1='2.00008' y1='369.114' stroke-width='2'/><line class='line3' x2='1' y2='369' x1='1' y1='198' stroke-width='2'/><line class='line2' x2='1' y2='99.9742' x1='1' y1='2.28882e-05' stroke-width='2'/> <line class='line1' x1='64' y1='1' y2='1' stroke-width='2'/></svg>";
+}, 4000);
 
+if ($(window).width() <= '1400') {
+  setInterval(function rewrite() {
+    let a = document.getElementById("svg-container").innerHTML = "<svg viewBox='0 0 1280 371' fill='none' xmlns='http://www.w3.org/2000/svg' class='header__svg' preserveAspectRatio='xMinYMin meet'><path  class='line7' d='M1098 133.303L1109.26 141.351L1109.25 125.247L1098 133.303Z'/><line class='line6' x2='1109' y2='133' x1='1278' y1='133' stroke-width='2'/><line class='line5' x1='1279' y1='368' x2='1279' y2='132' stroke-width='2'/><line class='line4' x2='1280' y2='369.114' x1='2.00008' y1='369.114' stroke-width='2'/><line class='line3' x2='1' y2='369' x1='1' y1='90' stroke-width='2'/><line class='line2' x2='1' y2='99.9742' x1='1' y1='2.28882e-05' stroke-width='2'/> <line class='line1' x1='64' y1='88' y2='88' stroke-width='2'/></svg>";
+  }, 4000);
+}
 });  
